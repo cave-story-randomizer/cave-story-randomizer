18158264cdae35b5fc5ef46458e4569c8cdcd840
shru <toromi@protonmail.com>
2018-12-18 23:06:41 -0500

    Added Randomizer class.

--- src/main.lua
+++ src/main.lua
@@ -20,17 +20,7 @@ function logNotice(...)  _log(3, 'NOTICE: ',  ...) end
 function logInfo(...)    _log(4, 'INFO: ',    ...) end
 function logDebug(...)   _log(5, 'DEBUG: ',   ...) end
 
-local TSC_FILES = {}
-do
-  local ITEM_DATA = require 'database.items'
-  for k, v in pairs(ITEM_DATA) do
-    local filename = v.map .. '.tsc'
-    if _.contains(TSC_FILES, filename) == false then
-      table.insert(TSC_FILES, filename)
-    end
-  end
-end
-
+local randomizer = require 'randomizer'()
 local background
 local font
 local screen
@@ -46,78 +36,7 @@ function love.load()
 end
 
 function love.directorydropped(path)
-  -- Mount.
-  local mountPath = 'mounted-data'
-  assert(lf.mount(path, mountPath))
-  local dirStage = '/' .. mountPath
-  do
-    local items = lf.getDirectoryItems(dirStage)
-    local containsData = _.contains(items, 'data')
-    if containsData then
-      dirStage = dirStage .. '/data'
-    end
-  end
-  do
-    local items = lf.getDirectoryItems(dirStage)
-    local containsStage = _.contains(items, 'Stage')
-    if containsStage then
-      dirStage = dirStage .. '/Stage'
-    else
-      status = "Could not find \"data\" subfolder.\n\nMaybe try dropping your Cave Story \"data\" folder in directly?"
-      return
-    end
-  end
-
-  -- Offer tribute to RNGesus.
-  local seed = tostring(os.time())
-  math.randomseed(seed)
-  logNotice(('Offering seed "%s" to RNGesus'):format(seed))
-
-  -- Create TscFile objects.
-  local tscFiles = {}
-  for _, filename in ipairs(TSC_FILES) do
-    local path = dirStage .. '/' .. filename
-    local TscFile = require 'tsc_file'
-    tscFiles[filename] = TscFile(path)
-  end
-
-  -- Create ItemDeck.
-  local ItemDeck = require 'item_deck'
-  local itemDeck = ItemDeck()
-
-  -- Place random weapon in either First Cave or Hermit Gunsmith.
-  local firstArea = _.sample({'Cave.tsc', 'Pole.tsc'})
-  tscFiles[firstArea]:replaceItem(itemDeck:getWeapon())
-
-  -- Replace all items.
-  for _, tscFile in pairs(tscFiles) do
-    while tscFile:hasUnreplacedItems() do
-      tscFile:replaceItem(itemDeck:getAny())
-    end
-  end
-
-  local sourcePath = lf.getSourceBaseDirectory()
-
-  -- Create /data/Stage if it doesn't already exist.
-  local command = ('mkdir "%s"'):format(sourcePath .. '/data/Stage')
-  os.execute(command) -- HERE BE DRAGONS!!!
-
-  -- Write modified files.
-  for filename, tscFile in pairs(tscFiles) do
-    local path = sourcePath .. '/data/Stage/' .. filename
-    tscFile:writeTo(path)
-  end
-
-  -- Unmount.
-  assert(lf.unmount(path))
-  print("\n")
-
-  -- Update status
-  status = [[Randomized data successfully created!
-
-Next overwrite the files in your copy of Cave Story with the versions in the newly created "data" folder. Don't forget to save a backup of the originals!
-
-Then play and have a fun!]]
+  status = randomizer:randomize(path)
 end
 
 function love.keypressed(key)
---
+++ src/randomizer.lua
@@ -0,0 +1,105 @@
+local ItemDeck = require 'item_deck'
+local TscFile  = require 'tsc_file'
+
+local C = Class:extend()
+
+local TSC_FILES = {}
+do
+  local ITEM_DATA = require 'database.items'
+  for k, v in pairs(ITEM_DATA) do
+    local filename = v.map .. '.tsc'
+    if _.contains(TSC_FILES, filename) == false then
+      table.insert(TSC_FILES, filename)
+    end
+  end
+end
+
+function C:randomize(path)
+  local success, dirStage = self:_mountDirectory(path)
+  if not success then
+    return "Could not find \"data\" subfolder.\n\nMaybe try dropping your Cave Story \"data\" folder in directly?"
+  end
+  self:_seedRngesus()
+  local tscFiles = self:_createTscFiles(dirStage)
+  self:_shuffleItems(tscFiles)
+  self:_writeModifiedData(tscFiles)
+  self:_unmountDirectory(path)
+  return [[Randomized data successfully created!
+
+Next overwrite the files in your copy of Cave Story with the versions in the newly created "data" folder. Don't forget to save a backup of the originals!
+
+Then play and have a fun!]]
+end
+
+function C:_mountDirectory(path)
+  local mountPath = 'mounted-data'
+  assert(lf.mount(path, mountPath))
+  local dirStage = '/' .. mountPath
+
+  local items = lf.getDirectoryItems(dirStage)
+  local containsData = _.contains(items, 'data')
+  if containsData then
+    dirStage = dirStage .. '/data'
+  end
+
+  local items = lf.getDirectoryItems(dirStage)
+  local containsStage = _.contains(items, 'Stage')
+  if containsStage then
+    dirStage = dirStage .. '/Stage'
+  else
+    return false, ''
+  end
+
+  return true, dirStage
+end
+
+function C:_seedRngesus()
+  local seed = tostring(os.time())
+  math.randomseed(seed)
+  logNotice(('Offering seed "%s" to RNGesus'):format(seed))
+end
+
+function C:_createTscFiles(dirStage)
+  local tscFiles = {}
+  for _, filename in ipairs(TSC_FILES) do
+    local path = dirStage .. '/' .. filename
+    tscFiles[filename] = TscFile(path)
+  end
+  return tscFiles
+end
+
+function C:_shuffleItems(tscFiles)
+  local itemDeck = ItemDeck()
+
+  -- Place random weapon in either First Cave or Hermit Gunsmith.
+  local firstArea = _.sample({'Cave.tsc', 'Pole.tsc'})
+  tscFiles[firstArea]:replaceItem(itemDeck:getWeapon())
+
+  -- Replace all items.
+  for _, tscFile in pairs(tscFiles) do
+    while tscFile:hasUnreplacedItems() do
+      tscFile:replaceItem(itemDeck:getAny())
+    end
+  end
+end
+
+function C:_writeModifiedData(tscFiles)
+  local sourcePath = lf.getSourceBaseDirectory()
+
+  -- Create /data/Stage if it doesn't already exist.
+  local command = ('mkdir "%s"'):format(sourcePath .. '/data/Stage')
+  os.execute(command) -- HERE BE DRAGONS!!!
+
+  -- Write modified files.
+  for filename, tscFile in pairs(tscFiles) do
+    local path = sourcePath .. '/data/Stage/' .. filename
+    tscFile:writeTo(path)
+  end
+end
+
+function C:_unmountDirectory(path)
+  assert(lf.unmount(path))
+  print("\n")
+end
+
+return C
