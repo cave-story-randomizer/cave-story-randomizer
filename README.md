db1ee6cf3ea64f9744dd437cb6c4d8c79353cb3d
shru <toromi@protonmail.com>
2018-12-19 16:13:29 -0500

    Handle Super Missile Launcher correctly.

--- README.md
+++ README.md
@@ -7,13 +7,16 @@ Todo
 - Print Warning/Error Count
 - Supress Hell Music Error
 - Write log
-- Test Getting Super Missiles Before Missiles
+- Support Cave Story+
+- Missiles should only replace items which are not part of cutscenes.
 - Trade Sequence Step B: Require random obtainable weapon, instead of always polar star and fireball.
 - Randomize Booster v0.8 / v2.0
 
 Issues
 ------
 
+- Items will no longer trigger associated cutscenes if replaced with missiles. This probably breaks something somewhere...
+- Collecting the Super Missile Launcher increases your maximum missiles by 5. This does not normally happen.
 - 3 Life Capsules can not be replaced because they appear on maps with 2 capsules. Need label-aware replace.
 - Hell Missile Upgrade uses a unique script and won't be easy to replace.
 - Trading back the Nemesis for the Blade almost certainly will be weird.
--- src/database/items.lua
+++ src/database/items.lua
@@ -333,13 +333,17 @@ local data = {
       "Got the =Super Missile Launcher=!<WAI0160<NOD", -- Gameplay text
       "Your Missiles have been powered up!<WAI0160<NOD", -- Actual text
     },
-    command = "<AM-0005<AM+0010:0000",
+    -- <AM+0005:0005 - Give the missile launcher and 5 ammo, just in case we don't have it yet.
+    --                 We need to give ammo because the missiles fire infinitely when max ammo is 0.
+    -- <FL+0202      - Sets the flag which tells the regular Missiles script that we have the Super Missiles.
+    command = "<AM+0005:0005<TAM0005:0010:0000<FL+0202",
     displayCmd = "<GIT0010",
     music = "<CMU0010",
     replaceBefore = {
       ["<FLJ0201:0201"] = "<EVE0201", -- Skip check which ensures you already have the missiles.
-      ["<TAM0005:"] = "<AM-0005<AM+",
+      ["<TAM0005:0010:0000"] = "<AM+0005:0005<TAM0005:0010:0000<FL+0202",
     },
+    erase = "<FL+0202",
     label = "0201", -- Also 0200...
   }
   -- !!!! Uses a unique script... will have to be crafty... !!!!
@@ -420,16 +424,6 @@ anyway.<NOD<END
 but you've already got the
 strongest weapon, so what
 else can I do?<NOD<END
-
-#0200
-<PRI<FLJ0201:0201<MSGA charm has been placed on it.
-It won't open...<NOD<END
-#0201
-<FLJ0766:0001<FL+0766<FL-0765<FL+0202
-<SOU0022<CNP0200:0021:0000
-<MSGOpened the treasure chest.<NOD<CLR<TAM0005:0010:0000
-<CMU0010<GIT0010Your Missiles have been powered up!<WAI0160<NOD<RMU<END
-
 
 #0302
 ...
@@ -549,5 +543,14 @@ Max health increased by 4!<NOD<END
 <PRI<FLJ0218:0001<FL+0218
 <SOU0022<CNP0302:0021:0000
 <MSGOpened the treasure chest.<NOD<EVE0030
+
+ #0200
+<PRI<FLJ0201:0201<MSGA charm has been placed on it.
+It won't open...<NOD<END
+#0201
+<FLJ0766:0001<FL+0766<FL-0765<FL+0202
+<SOU0022<CNP0200:0021:0000
+<MSGOpened the treasure chest.<NOD<CLR<TAM0005:0010:0000
+<CMU0010<GIT0010Your Missiles have been powered up!<WAI0160<NOD<RMU<END
 
 ]]
