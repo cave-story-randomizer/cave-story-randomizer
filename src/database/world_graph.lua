e52e63f8e8ef27e067eff2740fc4a2ca8c77417f
duncathan <dunc@duncathan.com>
2020-03-06 05:54:52 -0600

    guarantee arthur's key hint in camp/arthur mode

--- src/database/world_graph.lua
+++ src/database/world_graph.lua
@@ -613,6 +613,8 @@ function worldGraph:getHintableLocations(obj)
   for k, location in pairs(_.shuffle(self:getFilledLocations(true))) do
     if (obj == "objBadEnd" and location.item.name == "Rusty Key") or (obj ~= "objBadEnd" and location.item.name == "ID Card") then 
       table.insert(locations, 1, location) -- put that item on the top to guarantee a hint for it
+    elseif (self:Camp() or self:Arthur()) and location.item.name == "Arthur's Key" then
+      table.insert(locations, 1, location)
     elseif location:getPrebuiltHint() ~= nil then
       -- do nothing
     else
