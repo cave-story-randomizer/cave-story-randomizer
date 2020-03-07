a2347d60a3060a7f9a15ba57491af72b77f8cfd5
duncathan <dunc@duncathan.com>
2020-03-07 14:31:04 -0600

    lol

--- daily.sh
+++ daily.sh
@@ -7,5 +7,5 @@ zip -9 -r "${P}.love" .
 sudo xvfb-run --server-args="-screen 0 1024x768x24" love "${P}.love" --daily
 body=$(cat daily.txt)
 echo "$body"
-curl -H "Content-Type: application/json" -X POST -d '{content: "test"}' "$WEBHOOK"
+curl -H "Content-Type: application/json" -X POST -d '{"content": "test"}' "$WEBHOOK"
 curl -H "Content-Type: application/json" -X POST -d "$body" "$WEBHOOK"
