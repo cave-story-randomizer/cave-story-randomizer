a9c84affb7d94d211ae2ecd37f6be4d9fd310f9c
duncathan <dunc@duncathan.com>
2020-03-07 11:41:27 -0600

    yet another angle

--- daily.sh
+++ daily.sh
@@ -1,13 +1,17 @@
 
-#!/usr/bin/env bash
+!/usr/bin/env bash
 set -x
 
 P="csrando"
 
-#if [ TRAVIS_EVENT_TYPE != "cron"]
+#if [ TRAVIS_EVENT_TYPE ~= "cron"]
 #then
 #  exit
 #fi
+
+sudo add-apt-repository ppa:bartbes/love-stable
+sudo apt-get update
+sudo apt-get install love
 
 ##### build #####
 mkdir "target"
@@ -15,9 +19,12 @@ mkdir "target"
 ### .love
 cp -r src target	
 cd target/src
-"love-release -D -p ${P}"
-cd releases
-{$P.deb} --daily
+
+# .love file
+zip -9 -r - . > "../${P}.love"
+cd -
+
+xvfb-run love "target/${P}.love" --daily
 
 read -r body <daily.txt
 curl -H "Content-Type: application/json" -X POST -d "${body}" "${WEBHOOK}"
