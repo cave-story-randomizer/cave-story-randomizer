486a15deebc684d057983915e7ba483d41461ada
duncathan <dunc@duncathan.com>
2020-03-07 14:00:52 -0600

    YOUUUU AND IIIIIIIII

--- daily.sh
+++ daily.sh
@@ -1,23 +1,9 @@
-
-!/usr/bin/env bash
-set -x
-
 P="csrando"
-
-#if [ TRAVIS_EVENT_TYPE ~= "cron"]
-#then
-#  exit
-#fi
-
-##### build #####
-mkdir "target"
-
-### .love
-cp -r src target	
+mkdir target
+mkdir target/src
+cp -r src target
 cd target/src
-zip -9 -r - . > "../${P}.love"
-cd -
-sudo timeout -t 60 xvfb-run love "target/${P}.love" --daily
-
-read -r body <daily.txt
+zip -9 -r "${P}.love" .
+sudo xvfb-run --server-args="-screen 0 1024x768x24" love "${P}.love" --daily
+body=$(cat daily.txt)
 curl -H "Content-Type: application/json" -X POST -d "${body}" "${WEBHOOK}"
