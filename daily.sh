e77239387c03b49fa7b4286d9dbd2623b9c2b74a
duncathan salt <duncathan@duncathan.com>
2020-03-08 22:07:58 -0600

    working on travis builds (#92)
    * working on travis builds
    
    * fix
    
    * sudo
    
    * install libzip
    
    * ?
    
    * dev
    
    * hererocks
    
    * idk man
    
    * asda
    
    * reorder
    
    * source?
    
    * sudo again
    
    * etsts
    
    * why won't activate work
    
    * su
    
    * no su
    
    * manual path
    
    * whoops
    
    * use the proper build dir
    
    * set up deployment (hopefully)
    
    * proper release directory
    
    * jk what is it
    
    * should be this
    
    * forgot one
    
    Co-authored-by: duncathan <dunc@duncathan.com>

--- daily.sh
+++ daily.sh
@@ -1,10 +1,6 @@
+set -ev
+
 if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then exit 0; fi
-P="csrando"
-mkdir target
-mkdir target/src
-cp -r src target
-cd target/src
-zip -9 -r "${P}.love" .
-sudo xvfb-run --server-args="-screen 0 1024x768x24" love "${P}.love" --daily
+sudo xvfb-run --server-args="-screen 0 1024x768x24" love "releases/CaveStoryRandomizer.love" --daily
 cat daily.txt
 curl -H "Content-Type: application/json" -X POST -d @daily.txt "$WEBHOOK"
