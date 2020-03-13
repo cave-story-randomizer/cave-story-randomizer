e1b24f451d43fc3876efa2486e59c87b32685c01
duncathan <dunc@duncathan.com>
2020-03-13 02:46:23 -0600

    what happened to travis

--- daily.sh
+++ daily.sh
@@ -1,6 +1,10 @@
 set -ev
 
 if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then exit 0; fi
+sudo add-apt-repository -y ppa:bartbes/love-stable
+sudo apt-get -q update
+sudo apt-get -y install love
+
 cd src
 love-release
 love "releases/CaveStoryRandomizer.love" --daily --headless
