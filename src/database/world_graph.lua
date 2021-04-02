24fb2f6c8ef3b1ea9fd32dbad0b13475315fb3fe
duncathan <dunc@duncathan.com>
2021-04-01 20:03:37 -0600

    presses save in vscode
    (actually implements the softlock debug cats, lmao)

--- src/database/world_graph.lua
+++ src/database/world_graph.lua
@@ -546,11 +546,18 @@ function worldGraph:Camp() return self.spawn == "Camp" end
 
 function worldGraph:getSpawnScript()
   local initialHPCounter = "<FL+4011<FL+4012" -- initializes the HP counter to 3 HP
-  local mapFlags = "<MP+0040<MP+0043" -- Camp and Labyrinth B
-  if self:StartPoint() then return initialHPCounter .. mapFlags .. "<FL+6200<EVE0091" end
-  local earlyGameFlags = "<FL+0301<FL+0302<FL+1641<FL+1642<FL+0320<FL+0321"
-  if self:Arthur() then return initialHPCounter .. mapFlags .. "<FL+6201" .. earlyGameFlags .. "<TRA0001:0094:0008:0004" end
-  if self:Camp() then return initialHPCounter .. mapFlags .. "<FL+6202" .. earlyGameFlags .. "<TRA0040:0094:0014:0009" end
+  local mapFlagsCMP = "<MP+0040<MP+0043" -- Camp and Labyrinth B
+  local mapFlagsSoftlock = "<MP+0032<MP+0033<MP+0036" -- Small Room (Curly), Jenka's House (both)
+
+  local baseStartScript = initialHPCounter .. mapFlagsCMP .. mapFlagsSoftlock -- TODO: remove mapFlagsSoftlock when playing ER
+
+  if self:StartPoint() then return baseStartScript .. "<FL+6200<EVE0091" end
+
+  local earlyGameFlags = "<FL+0301<FL+0302<FL+1641<FL+1642<FL+0320<FL+0321" -- flags set during first cave in normal gameplay
+  baseStartScript = baseStartScript .. earlyGameFlags
+  
+  if self:Arthur() then return baseStartScript .. "<FL+6201<TRA0001:0094:0008:0004" end
+  if self:Camp() then return baseStartScript .. "<FL+6202<TRA0040:0094:0014:0009" end
 end
 
 function worldGraph:getLocations()
